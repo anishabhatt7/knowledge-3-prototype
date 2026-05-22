0c36b547a18a2f4bc7a98fe19b57b964031e8063
Nimit <nimit.khurana@gmail.com>
2026-05-22 11:50:31 +0530

    feat: restructure to Salesforce Knowledge article page layout

---
+++ src/modules/page/knowledgeArticle/knowledgeArticle.js
@@ -0,0 +1,5 @@
+import { LightningElement } from 'lwc';
+
+export default class KnowledgeArticle extends LightningElement {
+  // Static for now - will wire AI features in next tasks
+}
--- src/modules/shell/app/app.js
+++ src/modules/shell/app/app.js
@@ -1,57 +1,5 @@
-import { LightningElement, track } from 'lwc';
+import { LightningElement } from 'lwc';
 
 export default class App extends LightningElement {
-  @track activeTab = 'articleAnalyzer';
-
-  get isAnalyzerActive() {
-    return this.activeTab === 'articleAnalyzer';
-  }
-
-  get isAuthorActive() {
-    return this.activeTab === 'guidedAuthor';
-  }
-
-  get isMetadataActive() {
-    return this.activeTab === 'metadataSuggester';
-  }
-
-  get isEnrichmentActive() {
-    return this.activeTab === 'enrichmentPreview';
-  }
-
-  get isDashboardActive() {
-    return this.activeTab === 'healthDashboard';
-  }
-
-  get analyzerTabClass() {
-    return this.isAnalyzerActive ? 'slds-tabs_default__item slds-is-active' : 'slds-tabs_default__item';
-  }
-
-  get authorTabClass() {
-    return this.isAuthorActive ? 'slds-tabs_default__item slds-is-active' : 'slds-tabs_default__item';
-  }
-
-  get metadataTabClass() {
-    return this.isMetadataActive ? 'slds-tabs_default__item slds-is-active' : 'slds-tabs_default__item';
-  }
-
-  get enrichmentTabClass() {
-    return this.isEnrichmentActive ? 'slds-tabs_default__item slds-is-active' : 'slds-tabs_default__item';
-  }
-
-  get dashboardTabClass() {
-    return this.isDashboardActive ? 'slds-tabs_default__item slds-is-active' : 'slds-tabs_default__item';
-  }
-
-  handleTabClick(event) {
-    const tabName = event.currentTarget.dataset.tab;
-    this.activeTab = tabName;
-  }
-
-  handleNavigate(event) {
-    const { tab } = event.detail;
-    if (tab) {
-      this.activeTab = tab;
-    }
-  }
+  // Currently no tab state needed - just renders Knowledge article page
 }
