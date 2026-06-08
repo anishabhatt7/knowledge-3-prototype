a6e277e022435ec37a39be9c0cfe718a32a93cd8
bhattanisha21-beep <anisha.bhatt+sfemu@salesforce.com>
2026-06-08 16:18:05 +0530

    Deploy pm prototype to /pm

---
+++ pm/assets/main-BRaduy37.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./bootstrap-BFyMKiht.js","./preload-helper-PPVm8Dsz.js","./empty_html-CccsVKgC.js","./slds-loader-Co_8t9Qh.js"])))=>i.map(i=>d[i]);
+import"./index-CentR3O6.js";import{_ as r}from"./preload-helper-PPVm8Dsz.js";await r(()=>import("./bootstrap-BFyMKiht.js"),__vite__mapDeps([0,1,2,3]),import.meta.url);
